e0c8351e2b20df969ad762e65ee259b2a721eef1
freakyInsane <vlad.petrishin@gmail.com>
2017-09-02 21:41:10 +0300

    test commit for Taras pulling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // test test
 
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +11,30 @@ app.get('/', function (req, res) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});*/
+
+var express = require("express"); 
+var site = express();
+
+site.set('port', (process.env.PORT || 5000));
+
+//var site = express.createServer(); 
+var fill = require('./spooky_fill.js');
+
+site.use(express.static(__dirname + '/..'));
+
+site.get("/", function(req, res) {   
+	fill.search;
+	//fs.createReadStream("./index.html").pipe(res); 
 });
 
+//site.get('/fill', fill.search);
+
+site.listen(site.get('port'), function() {
+  console.log('Node app is running on port', site.get('port'));
+});
+
+//site.listen(3000);
+
+//console.log("Server listening on http://localhost:9201");
 
