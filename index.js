6e9577f78730c6b47af34707898c5e37ac26b25a
Taras Lysenko <sarasenly@gmail.com>
2017-09-02 22:43:46 +0300

    add basic setup

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-// test test
+const express = require('express');
+const app = express();
+const osmosis = require('osmosis');
 
-/*var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+    osmosis
+        .get('www.google.com')
+        .set({'Title': 'title'})   // альтернатива: `.find('title').set('Title')`
+        .data(console.log); // выведет {'Title': 'Google'}
+    res.send('Hello World!');
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});*/
-
-var express = require("express"); 
-var site = express();
-
-site.set('port', (process.env.PORT || 5000));
-
-//var site = express.createServer(); 
-var fill = require('./spooky_fill.js');
-
-site.use(express.static(__dirname + '/..'));
-
-site.get("/", function(req, res) {   
-	fill.search;
-	//fs.createReadStream("./index.html").pipe(res); 
+app.listen(7777, () => {
+    console.log('Example app listening on port 7777!');
 });
-
-//site.get('/fill', fill.search);
-
-site.listen(site.get('port'), function() {
-  console.log('Node app is running on port', site.get('port'));
-});
-
-//site.listen(3000);
-
-//console.log("Server listening on http://localhost:9201");
 
